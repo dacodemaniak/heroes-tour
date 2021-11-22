12fac1f621df5dcdd4142f05f721e9f680ff56ed
DaCodeManiaK <jla.webprojet@gmail.com>
2021-11-22 13:07:44 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'heroes-tour';
+export class AppComponent implements OnInit {
+  /**
+   * main title
+   * @var string
+   */
+  public title!: string;
+
+  public heroes: string[];
+
+  public constructor() {
+    this.heroes = [];
+  }
+
+  ngOnInit(): void {
+    this.title = 'Heroes Tour';
+
+    this.heroes.push('Superman');
+    this.heroes.push('Batman');
+    this.heroes.push('Spiderman');
+  }
 }
